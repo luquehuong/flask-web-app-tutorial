f58f4719198551196a808bc09fc102a0460ce3a4
Que Huong <quehuong@SE-Mac-Mini-Huong.local>
2019-05-09 16:55:16 +0700

    chapter 5 - completed

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.models import User, Post
+from app.forms import LoginForm, RegistrationForm
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
@@ -22,6 +23,10 @@ def index():
     ]
     return render_template('index.html', title='Home Page', posts=posts)
 
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -43,3 +48,17 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
