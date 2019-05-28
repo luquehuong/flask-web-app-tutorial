7420b895bb67788d263057fcf9c460ff48603907
Que Huong <quehuong@SE-Mac-Mini-Huong.local>
2019-05-28 16:09:22 +0700

    fix: some typos in chapter 10

--- app/__init__.py
+++ app/__init__.py
@@ -29,12 +29,12 @@ if not app.debug:
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
             fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMIN'], subject='Microblog Failure',
+            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
         if not os.path.exists('logs'):
-            os.mkdir(logs)
+            os.mkdir('logs')
         file_handler = RotatingFileHandler('logs/microblog.log', maxBytes=10240,
                                                     backupCount=10)
         file_handler.setFormatter(logging.Formatter(
